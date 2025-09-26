f1910db28d8643ae2742ed56f319f6be5ff40fec
ibarbylev <ibarbylev@gmail.com>
2025-09-26 10:00:01 +0300

    Initial commit

---
+++ practice_work_02.py
@@ -0,0 +1,14 @@
+""" 2.1 Подсчёт количества элемента
+
+Дан список покупок.
+Найдите, сколько раз в списке встречается товар "Banana".
+
+Данные:
+shopping_list = ["Banana", "Apple", "Banana", "Orange", "Banana", "Apple"]
+Пример вывода:
+Товар "Banana" встречается 3 раз(а).
+
+"""
+
+shopping_list = ["Banana", "Apple", "Banana", "Orange", "Banana", "Apple"]
+
